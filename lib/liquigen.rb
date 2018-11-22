5fb378261842bc0e0aba3cd093afdb6930d590a6
Jeremy Cui <tsuijy@gmail.com>
2018-11-22 13:48:14 +0800

    Add config command to allow set application configuration

--- lib/liquigen.rb
+++ lib/liquigen.rb
@@ -27,5 +27,23 @@ require 'liquigen/handlers/sql'
 require 'liquigen/handlers/add_index'
 require 'liquigen/handlers/drop_table'
 
+require 'liquigen/scaffold/config'
+
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
+
+module Liquigen
+  class << self
+    attr_accessor :package_name
+    # default value: #{package_name}.controller
+    attr_accessor :controller_package_name
+    # default value: #{package_name}.model
+    attr_accessor :entity_package_name
+    # default value: #{package_name}.repository
+    attr_accessor :repository_package_name
+    # default value: src/main/java
+    attr_accessor :java_codes_root
+    # default value: src/main/resources/db/migrations
+    attr_accessor :migration_path
+  end
+end
---
+++ lib/liquigen/scaffold/config.rb
@@ -0,0 +1,25 @@
+module Liquigen::Scaffold
+  CONFIG_FILE = '.liquigen'.freeze
+  class Config
+    # write config file
+    def process
+      # if not exist the .liquigen file create it
+      File.write(CONFIG_FILE, prepare_default_content.join("\n")) unless File.exist?(CONFIG_FILE)
+
+      # then open the vim editor
+      system('vi ' + CONFIG_FILE)
+    end
+
+    def prepare_default_content
+      contents = []
+      contents << '# You can remove the # to set your custom value'
+      contents << '# package_name=your-package-name, required'
+      contents << '# java_codes_root=src/main/java'
+      contents << '# migration_path=src/main/resources/db/migrations'
+      contents << '# controller_package_name='
+      contents << '# entity_package_name='
+      contents << '# repository_package_name='
+      contents << ''
+    end
+  end
+end
